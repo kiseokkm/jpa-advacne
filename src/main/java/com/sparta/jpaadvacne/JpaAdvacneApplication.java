ea1a81bf14138019684ffca32bb5179614921fd1
kiseokkm <311kskim@gmail.com>
2024-05-23 14:38:24 +0900

    Feat : Initial commit

---
+++ src/main/java/com/sparta/jpaadvacne/JpaAdvacneApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.jpaadvacne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAdvacneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaAdvacneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/jpaadvacne/JpaAdvacneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.jpaadvacne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAdvacneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
